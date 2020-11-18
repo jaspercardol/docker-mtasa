739663037d768678514e6ff4c725aaab93dc7dfc
jaspercardol <37154053+jaspercardol@users.noreply.github.com>
2020-11-18 12:11:08 +0100

    Added all code to entrypoint instead of compose
    - Can now check if packages are installed and will install them if not
    - If /config is empty, it will download the necessary files

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-cd /mta
+apk info -vv | grep 'iproute2'
+if [[ $? -eq 1 ]]; then
+  apk update
+  apk add tar curl iproute2 openssl wget unzip
+
+if [ -z "$(ls -A /config)" ]; then # Is /config empty?
+    mkdir /config/tmp
+    cd /config/tmp
+    wget https://linux.mtasa.com/dl/multitheftauto_linux_x64.tar.gz
+    tar -xf multitheftauto_linux_x64.tar.gz
+    cd multitheftauto_linux_x64
+    mv * /config
+    cd /config
+    rm -rf tmp
+    
+
 ./mta-server64
